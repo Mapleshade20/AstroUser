c454eff4f0cd8ee308775b16ed3b94d8863f31b6
MapleShade <maple@MapleBook.local>
2024-06-28 14:50:21 +0800

    Make fonts larger on Mac

--- plugins/neovide.lua
+++ plugins/neovide.lua
@@ -2,6 +2,32 @@ if not vim.g.neovide then
   return {} -- do nothing if not in a Neovide session
 end
 
+if package.config:sub(1, 1) == "\\" then
+  return {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      options = {
+        opt = { -- configure vim.opt options
+          -- configure font
+          guifont = "JetBrainsMono Nerd Font:h13",
+          -- line spacing
+          -- linespace = 0,
+        },
+        g = { -- configure vim.g variables
+          -- configure scaling
+          -- neovide_scale_factor = 1.0,
+          -- configure padding
+          -- neovide_padding_top = 0,
+          -- neovide_padding_bottom = 0,
+          -- neovide_padding_right = 0,
+          -- neovide_padding_left = 0,
+        },
+      },
+    },
+  }
+end
+
 return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
@@ -9,7 +35,7 @@ return {
     options = {
       opt = { -- configure vim.opt options
         -- configure font
-        guifont = "JetBrainsMono Nerd Font:h13",
+        guifont = "JetBrainsMono Nerd Font:h17",
         -- line spacing
         -- linespace = 0,
       },
