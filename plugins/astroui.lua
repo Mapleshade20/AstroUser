77bc55a1aa57e93d33f5b2475b7d50979a588780
MapleShade20 <howard380mc@outlook.com>
2024-07-13 16:07:47 +0800

    feat: add transparency

--- plugins/astroui.lua
+++ plugins/astroui.lua
@@ -6,17 +6,29 @@
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
+  lazy = false,
+  priority = 10000,
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
     colorscheme = "astrodark",
+    style = {
+      transparent = true,
+      float = false,
+      neotree = false,
+      inactive = false,
+    },
+    -- terminal_color = false,
+
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+        Normal = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+        NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
+        -- Normal = { bg = "NONE" },
       },
     },
     -- Icons can be configured throughout the interface
---
+++ plugins/cyberdream.lua
@@ -0,0 +1,10 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  "scottmckendry/cyberdream.nvim",
+  name = "cyberdream",
+  lazy = false,
+  opts = {
+    transparent = true,
+    italic_comments = true,
+  },
+}
